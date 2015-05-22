826785f66d1cf4a3850e8636350705ecaaa32ea5
Dwight Hubbard <d@dhub.me>
2015-05-21 21:02:37 -0700

    Include the metadata in the package

--- serviceping/__init__.py
+++ serviceping/__init__.py
@@ -26,7 +26,7 @@ __ci_build_number__ = str(_package_metadata.get('ci_build_number', ''))
 __git_branch__ = str(_package_metadata.get('git_branch', ''))
 __git_origin__ = str(_package_metadata.get('git_origin', ''))
 __git_hash__ = str(_package_metadata.get('git_hash', ''))
-__git_base_url__ = 'https://github.com/yahoo/redislite'
+__git_base_url__ = 'https://github.com/yahoo/serviceping'
 if __git_origin__.endswith('.git'):  # pragma: no cover
     __git_base_url__ = __git_origin__[:-4].strip('/')
 __source_url__ = __git_base_url__ + '/tree/' + __git_hash__
