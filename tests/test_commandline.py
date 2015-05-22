7768232453916adbc81dba7e2da3ae673b5f9df5
Dwight Hubbard <d@dhub.me>
2015-05-21 21:52:48 -0700

    Switch to using argparse
    Add a test for the parsing code.

--- tests/test_commandline.py
+++ tests/test_commandline.py
@@ -9,7 +9,8 @@ test_serviceping
 
 Tests for `serviceping` module.
 """
-import serviceping
+import serviceping.commandline
+import sys
 import unittest
 
 
@@ -17,14 +18,14 @@ import unittest
 # when the the class is run (by calling unittest.main()
 class TestServiceping(unittest.TestCase):
 
-    def test_serviceping_calc_deviation(self):
-        times = [24.913, 25.120, 25.266, 26.371, 25.237, 24.833, 26.465, 25.053, 25.857, 25.121]
-        avg_time = sum(times) / float(len(times))
-        print(avg_time)
-        print(len(times))
-        print(serviceping.StatsList(times).standard_deviation())
-        result = serviceping.calc_deviation(times, avg_time)
-        self.assertAlmostEqual(result, 0.232, places=3)
+    def test_commandline(self):
+        sys.argv = ['serviceping', 'yahoo.com']
+        options, args = serviceping.commandline.parse_arguments()
+        print(options)
+        print(args)
+        self.assertEqual(args[0], 'yahoo.com')
+        self.assertEqual(options.count, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
