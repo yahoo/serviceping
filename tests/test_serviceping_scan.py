60dc08dbdf8be0fd0e1c2e1b966f86fa85168501
Dwight Hubbard <d@dhub.me>
2017-01-20 11:23:16 -0800

    Create test_serviceping_scan.py

---
+++ tests/test_serviceping_scan.py
@@ -0,0 +1,24 @@
+
+#!/usr/bin/env python
+# Copyright (c) 2013-2015, Yahoo Inc.
+# Copyrights licensed under the Apache 2.0 License
+# See the accompanying LICENSE.txt file for terms.
+
+"""
+test_serviceping
+----------------------------------
+Tests for `serviceping` module.
+"""
+from serviceping.scan import scan
+import unittest
+
+
+# Any methods of the class below that begin with "test" will be executed
+# when the the class is run (by calling unittest.main()
+class TestServicepingScan(unittest.TestCase):
+
+    def test_serviceping_scan_exception(self):
+        result =
+
+if __name__ == '__main__':
+unittest.main()
